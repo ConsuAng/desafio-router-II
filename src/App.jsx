08ebce93f456e4580cd4cb74c8c82b766b8e4585
Consue <c.anguitasalinas@gmail.com>
2022-10-07 15:29:43 -0300

    ✨ feat: Not found component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Detail from "./pages/Detail";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 import Pokemones from "./pages/Pokemones";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />}/>
           <Route path="pokemones" element={<Pokemones />}/>
           <Route path="pokemones/:name" element={<Detail />} />
+          <Route path="*" element={<NotFound />}/>
         </Routes>
         <Footer />
       </BrowserRouter>
