43a94764342bc155c0f503a8f63e6f4dc0dd4ecc
Consue <c.anguitasalinas@gmail.com>
2022-10-07 15:25:48 -0300

    ✨ feat: pages assembly

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Detail from "./pages/Detail";
+import Home from "./pages/Home";
+import Pokemones from "./pages/Pokemones";
+
 function App() {
 
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="pokemones" element={<Pokemones />}/>
+          <Route path="pokemones/:name" element={<Detail />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   )
 }
